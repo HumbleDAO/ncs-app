17f4aec567fc56ed430716be5a5ad3ac562569d1
JoelCCodes <32679235+JoelCCodes@users.noreply.github.com>
2022-09-05 20:02:55 -0300

    cleaned up subscriptions.

---
+++ packages/hardhat/contracts/NCSNFT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+contract NCSNFT is ERC721 {
+    uint256 tokenId;
+
+    constructor(string memory _name, string memory _symbol) public ERC721(_name, _symbol) {
+        tokenId = 0;
+    }
+
+    function mint(address _receiver) external returns (uint256) {
+        uint256 newId = tokenId + 1;
+        tokenId++;
+        _mint(_receiver, newId);
+        return newId;
+    }
+}
