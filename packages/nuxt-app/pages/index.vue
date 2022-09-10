f055fd94355cf30cb3846cef1305a9a27178dc06
Hugo <hugoce17@gmail.com>
2022-09-09 23:51:56 -0500

    fixed issue where app would break when user would connect to chainid that is not supported

--- packages/nuxt-app/pages/index.vue
+++ packages/nuxt-app/pages/index.vue
@@ -21,7 +21,7 @@
             <br />
             <div v-if="chain">Connected to {{ chain.name }}</div>
             <br />
-            <div v-if="contractsAddresses.length">
+            <div v-if="contractsAddresses && contractsAddresses.length">
                 <div v-for="contract in contractsAddresses" :key="contract.name" class="flex flex-col">
                     <label for="contract-name">
                         {{ contract.name }}
@@ -51,7 +51,7 @@ const colorMode = useColorMode()
 
 const { loadContracts } = useContractsStore()
 
-const { connect } = useConnect({
+const { connect, isConnected } = useConnect({
     connector: new InjectedConnector(),
 })
 
@@ -71,8 +71,9 @@ const { chain } = useNetwork()
 const { chains, error, isLoading, pendingChainId, switchNetwork } = useSwitchNetwork({
     chainId: 80001,
     onSuccess: async (data) => {
-        const loadedContracts = await loadContracts()
-        console.log('LOADED_CONTRACTS_ON_CURRENT_CHAIN: \n', loadedContracts.value.NCSubscriptionFactory.address)
+        // const loadedContracts = await loadContracts()
+        // console.log('LOADED_CONTRACTS_ON_CURRENT_CHAIN: \n', loadedContracts.value.NCSubscriptionFactory.address)
+        await loadContracts()
         useNetworkDetailsStore().$patch((state: INetworkDetails) => {
             state.selectedChainId = data.id
             state.network = data.network
@@ -82,20 +83,19 @@ const { chains, error, isLoading, pendingChainId, switchNetwork } = useSwitchNet
 
 useNetworkDetailsStore().$patch((state: INetworkDetails) => {
     state.network = chain.value?.name
-
     state.selectedChainId = chain.value?.id
     state.availableChains = chains.value
 })
 
-const loadedContracts = await loadContracts()
-console.log('INIT_LOADED CONTRACTS', loadedContracts)
-const { NCSubscriptionFactory } = loadedContracts.value
-console.log('NCSubscriptionFactory', NCSubscriptionFactory.address)
+// console.log('INIT_LOADED CONTRACTS', loadedContracts)
+// const { NCSubscriptionFactory } = loadedContracts.value
+// console.log('NCSubscriptionFactory', NCSubscriptionFactory.address)
 
-const contractsAddresses = computed(() =>
+const contractsAddresses = computed(async () => {
+    const loadedContracts = await loadContracts()
     Object.keys(loadedContracts.value).map((key) => ({
         name: key,
         address: loadedContracts.value[key].address,
     }))
-)
+})
 </script>
